ed6e513efbfa3b601b22daebe8335583fad87ea0
Yuyang Zhou <zhouyuyang712@outlook.com>
2023-10-10 01:47:43 +0000

    add loop for all file process

--- convert_csv/run.sh
+++ convert_csv/run.sh
@@ -1,8 +1,28 @@
 cd /shanjunjie/ProteinMultiClass/convert_csv
-python convert_csv.py \
-    --dbtl_path /shanjunjie/protein/dtbl_files/bacteria.nonredundant_protein.1.protein.dtbl \
-    --fasta_path /shanjunjie/protein/fasta/bacteria.nonredundant_protein.1.fasta \
-    --output_full_path /shanjunjie/ProteinMultiClass/convert_csv/csv/hmm_result/full/bacteria.nonredundant_protein.1.csv \
-    --output_seq_path /shanjunjie/ProteinMultiClass/convert_csv/csv/hmm_result/seq/bacteria.nonredundant_protein.1.csv
-python csv_inference.py \
-    --inference_dataset /shanjunjie/ProteinMultiClass/convert_csv/csv/hmm_result/seq/bacteria.nonredundant_protein.1.csv
+
+# Loop through each .faa file in the directory
+for file in "/shanjunjie/protein/dtbl_files"/*.dtbl
+do
+    # Extract the directory and the base file name without extension
+    dir=$(dirname "$file")
+    base=$(basename "$file" .dtbl)
+
+    dbtl_path="$file"
+    fasta_path="/shanjunjie/protein/fasta/${base}.faa"
+    output_full_path="/shanjunjie/ProteinMultiClass/convert_csv/csv/hmm_result/full/${base}.csv"
+    output_seq_path="/shanjunjie/ProteinMultiClass/convert_csv/csv/hmm_result/seq/${base}.csv"
+    inference_dataset="/shanjunjie/ProteinMultiClass/convert_csv/csv/hmm_result/seq/${base}.csv"
+    part_result_path="/shanjunjie/ProteinMultiClass/convert_csv/csv/result/${base}.part.csv"
+    full_result_path="/shanjunjie/ProteinMultiClass/convert_csv/csv/result/${base}.full.csv"
+
+    python convert_csv.py \
+        --dbtl_path "$dbtl_path" \
+        --fasta_path "$fasta_path" \
+        --output_full_path "$output_full_path" \
+        --output_seq_path "$output_seq_path"
+
+    python csv_inference.py \
+        --inference_dataset "$inference_dataset" \
+        --part_result_path "$part_result_path" \
+        --full_result_path "$full_result_path"
+done
